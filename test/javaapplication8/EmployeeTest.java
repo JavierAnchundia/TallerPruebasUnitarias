cee796397b1727a93d35bef6144055ce3373e807
Mishell <xmichelle26x@gmail.com>
2019-08-14 11:23:56 -0500

    Test de  Cs

--- test/javaapplication8/EmployeeTest.java
+++ test/javaapplication8/EmployeeTest.java
@@ -5,7 +5,13 @@
  */
 package javaapplication8;
 
+import java.time.ZoneId;
+import java.util.Date;
 import javaapplication8.Employee.EmployeeType;
+import static javaapplication8.Employee.EmployeeType.Manager;
+import static javaapplication8.Employee.EmployeeType.Supervisor;
+import static javaapplication8.Employee.EmployeeType.Worker;
+import static junit.framework.TestCase.assertEquals;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -55,7 +61,22 @@ public class EmployeeTest {
        assertNotEquals(386.0F, e.CalculateYearBonus(),1);
        
     }
-    
+     public void testCs() {
+        System.out.println("cs");
+        Employee instance = new Employee(100,"UKD", (float) 10.5,Worker);
+        Employee instance2 = new Employee(100,"UKD", (float) 10.5,Manager);  
+        Employee instance3 = new Employee(100,"UKD", (float) 10.5,Supervisor);  
+
+        float esperado=  (float) (instance.getSalary()*0.95);
+        Date date = new Date();
+        int month = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate().getMonthValue();
+        //Compruebo que con currency que diferente a USD, devuelva los valores esperados tanto para  Worker como para Manager y para Supervisor
+        assertEquals(95, instance.cs(), 0.0);
+        assertEquals(95+(instance2.getBonusPercentage() * 0.7F), instance2.cs(), 0.0);
+        assertEquals(95+(instance3.getBonusPercentage() * 0.35F), instance3.cs(), 0.0);
+
+        // TODO review the generated test code and remove the default call to fail.
+    }
     
     @Test
     public void testSalario0Manager() {
